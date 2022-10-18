ff4bf8cb23e0e3a32b25e5726018f7c794107ba0
VipulGajbhiye09 <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-10-18 21:12:29 +0530

    Add files via upload

---
+++ comparator.js
@@ -0,0 +1,18 @@
+//Difference between === and ==
+
+var a=1;
+var b="1";
+console.log("Output of ===")
+if(a===b){
+    console.log("yes")
+}
+else{
+    console.log("No")
+}
+console.log("Output of ==")
+if(a==b){
+    console.log("yes")
+}
+else{
+    console.log("No")
+}
---
+++ if_else.js
@@ -0,0 +1,9 @@
+var score = Math.random();
+score=score*100
+if(score > 50){
+    console.log("You Passed")
+}
+else{
+    console.log("You couldn't Passed")
+}
+console.log(score);
