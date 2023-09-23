9218d921f7b3760c155c477105a17837c709a218
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-23 23:12:57 +0530

    Update short _circuit.js

--- short _circuit.js
+++ short _circuit.js
@@ -9,3 +9,12 @@ const y = 1;
 console.log(x && y);  // Output: 0 (the first operand is falsy, so it is returned)
 console.log(y && x);  // Output: 0 (the second operand is falsy, so it is returned)
 
+-The || operator (logical OR) returns the first operand if it is "truthy", or the second operand if the first operand is "falsy".
+
+For example:
+
+const x = 0;
+const y = 1;
+
+console.log(x || y); // Output: 1 (the first operand is falsy, so the second operand is returned)
+console.log(y || x); // Output: 1 (the first operand is truthy, so it is returned)
