00da8f0dac66d416de6a152ca110147ba2a461de
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-10-08 23:50:26 +0530

    Create axios_01.js

---
+++ axios_01.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+const url = 'enter_url_herr'
+
+// Make a get request 
+axios.get(url)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
