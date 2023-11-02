992ee8f605658548a0eb9aa2844dc4cd0600819d
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-11-02 23:01:18 +0530

    Create map-01.js

---
+++ Map/map-01.js
@@ -0,0 +1,35 @@
+// Map returns a new array
+// does not change the size of original array (unlike filter)
+// uses values from original array when making new one
+
+const people = [
+  {
+    name: 'mike',
+    age: 25,
+    position: 'developer',
+  },
+  {
+    name: 'hulie',
+    age: 21,
+    position: 'designer',
+  },
+  {
+    name: 'ellie',
+    age: 24,
+    position: 'tech lead',
+  },
+  {
+    name: 'johnathan',
+    age: 30,
+    position: 'manager',
+  },
+];
+
+
+const newPeople = people.map((item) => {
+  return {
+    firsName: item.name.toUpperCase(),
+    oldAge: item.age + 20,
+  };
+});
+console.log(newPeople);
