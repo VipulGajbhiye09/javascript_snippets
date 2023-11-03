204f44194d6e5ed358a3f7fd330f0c4d98baa23a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-11-03 22:50:25 +0530

    Create filter-01.js

---
+++ Filter/filter-01.js
@@ -0,0 +1,31 @@
+//Filter - returns a new array, can manipulate the size of new array (unlike map), returns based on condition
+
+const people = [
+  {
+    name: 'mike',
+    age: 25,
+    position: 'developer',
+  },
+  {
+    name: 'hulie',
+    age: 21,
+    position: 'designer',
+  },
+  {
+    name: 'ellie',
+    age: 26,
+    position: 'tech lead',
+  },
+  {
+    name: 'johnathan',
+    age: 30,
+    position: 'manager',
+  },
+];
+const youngPeople = people.filter((person) => {
+  // if (person.age < 30) {
+  //   return person;
+  // }
+  return person.age <= 25;
+});
+console.log(youngPeople);
