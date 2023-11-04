c6e771421df1d9c36562a28c2e0c7d978e5a63c2
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-11-04 23:03:57 +0530

    Create find-01.js

---
+++ Find/find-01.js
@@ -0,0 +1,26 @@
+// Find - returns single instance (object), returns first match, if no match found - undefined
+const people = [
+  {
+    name: 'mike',
+    age: 25,
+    position: 'developer',
+  },
+  {
+    name: 'julie',
+    age: 21,
+    position: 'designer',
+  },
+  {
+    name: 'ellie',
+    age: 26,
+    position: 'tech lead',
+  },
+  {
+    name: 'johnathan',
+    age: 30,
+    position: 'manager',
+  },
+];
+
+const ellie = people.find((person) => person.name === 'ellie');
+console.log(ellie);
