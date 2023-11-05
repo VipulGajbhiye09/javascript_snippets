e85acf72fe3bf7a8f76e983bfa3aa9e84c979cc7
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-11-05 23:14:05 +0530

    Update fetch-01.js

--- Fetch/fetch-01.js
+++ Fetch/fetch-01.js
@@ -1,3 +1,7 @@
+// Fetch API -  Browser API for HTTP (AJAX) Requests
+// Default - GET Requests, supports other methods as well
+// Returns Promise so Async/Await is required
+
 const url = 'enter_url_endpoint';
 
 const fetch_data = async () => {
