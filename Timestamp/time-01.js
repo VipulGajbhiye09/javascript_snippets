01b8ee08ccc6626c84908464c622f7bcb918dba8
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-11-08 22:56:54 +0530

    Create time-01.js

---
+++ Timestamp/time-01.js
@@ -0,0 +1,10 @@
+// Date.now()
+console.log(Date.now());
+// new Date().getTime()
+console.log(new Date().getTime());
+// new Date().valueOf()
+console.log(new Date().valueOf());
+
+setTimeout(() => {
+  console.log(Date.now());
+}, 1000);
