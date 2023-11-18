6ec795f6be6c5a332b19f68a85dac0f6b4f4c26f
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-11-18 21:50:11 +0530

    Create optional-chaining-01.js

---
+++ Optional-Chaining/optional-chaining-01.js
@@ -0,0 +1,13 @@
+const people = [
+  {
+    name: 'mike',
+    location: { street: '17 Main street', timezone: +2:00'  },
+  },
+  { 
+    name: 'jason',
+   location: { street: '42 Filo street' } },
+  {
+    name: 'sarah',
+    location: { street: '4 Brie street', timezone:'+9:30'} },
+  },
+];
