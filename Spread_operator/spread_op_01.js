5d9ed155c3924f9cc494afa9be2b52de9576ed8c
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2024-02-07 19:24:50 +0530

    Update spread_op_01.js

--- Spread_operator/spread_op_01.js
+++ Spread_operator/spread_op_01.js
@@ -9,3 +9,22 @@ let arr2 = [4, 5];
  
 let arr = [...arr, ...arr2];
 console.log(arr); // [ 1, 2, 3, 4, 5 ]
+
+// if we want to expand an array into another
+
+// normally we expand like so
+let arr = ['a', 'b'];
+let arr2 = [arr, 'c', 'd'];
+ 
+console.log(arr2); 
+// [ [ 'a', 'b' ], 'c', 'd' ]
+// Even though we get the content on one array inside the other one, actually it is an array inside another array which is definitely what we didn’t want.
+// If we want the content to be inside a single array we can make use of the spread operator.
+
+// expanding using spread operator
+ 
+let arr = ['a', 'b'];
+let arr2 = [...arr, 'c', 'd'];
+ 
+console.log(arr2); 
+// [ 'a', 'b', 'c', 'd' ]
